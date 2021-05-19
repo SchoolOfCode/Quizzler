9bbec7ca7a653735f1e3848f325d8c0f69fa6e3b
Toby Smith <toby.d.smith@outlook.com>
2021-05-19 14:35:58 +0100

    Found an image API and got it working

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello There Team!");
+async function getImage() {
+  let image = await fetch(
+    `https://bing-image-search1.p.rapidapi.com/images/search?q=${searchValue}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "9ec856775cmsh18791d11e3541e2p1eeaf3jsn760b3c3d7241",
+        "x-rapidapi-host": "bing-image-search1.p.rapidapi.com",
+      },
+    }
+  );
+  myImage = await image.json();
+  const newImage = document.createElement("img");
+  newImage.src = myImage.value[0].thumbnailUrl;
+  document.body.appendChild(newImage);
+  console.log(myImage);
+}
+const searchValue = "Maths";
+getImage();
