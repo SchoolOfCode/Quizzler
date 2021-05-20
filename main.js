c3edc2e775fc178346de0429a77a3722794069b1
TaniaY21 <taniay@hotmail.co.uk>
2021-05-20 20:51:54 +0100

    string literal testing

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log("testing tania's branch");
+
+// base this on user input 
+let numOfQuestions = 10;
+let category = 9;
+let difficulty = "easy";
+let type = "multiple";
+let url = `https://opentdb.com/api.php?amount=${numOfQuestions}&category=${category}&difficulty=${difficulty}&type=${type}`;
+
+console.log(url);
+
+async function testAPI() {
+  let request = await fetch(url);
+  let data = request.json();
+  console.log(data);
+}
+
+testAPI();
+
