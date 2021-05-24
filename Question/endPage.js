20bf9412d31d8470495da9137a773801c5f0cd85
TaniaY21 <taniay@hotmail.co.uk>
2021-05-24 14:15:21 +0100

    css for endpage with lottie file

--- Question/endPage.js
+++ Question/endPage.js
@@ -4,7 +4,7 @@ console.log(gameScore);
 let scoreDisplay = document.getElementById("score");
 scoreDisplay.innerText = gameScore;
 
-const return_homePage_btn = document.getElementById("return-home");
+const return_homePage_btn = document.getElementById("return-home-btn");
 return_homePage_btn.innerText = "Back to main page";
 return_homePage_btn.addEventListener(
   "click",
