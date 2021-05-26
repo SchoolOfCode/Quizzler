2116c5b1017107444759f1a3d49a6dc712c6f1a7
Toby Smith <toby.d.smith@outlook.com>
2021-05-26 13:43:42 +0100

    Made a dad joke at the end of the end page

--- Question/endPage.js
+++ Question/endPage.js
@@ -6,7 +6,16 @@ scoreDisplay.innerText = gameScore;
 
 const return_homePage_btn = document.getElementById("return-home-btn");
 return_homePage_btn.innerText = "Back to main page";
-return_homePage_btn.addEventListener(
-  "click",
-  () => (window.location.href = "../Home/index.html")
-);
+return_homePage_btn.addEventListener("click", () => (window.location.href = "../Home/index.html"));
+
+async function createDadJoke() {
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    headers: { accept: "application/json" },
+  });
+  const jokes = await response.json();
+  const dadJokeSection = document.querySelector(".dad-jokes");
+  let dadJoke = document.createElement("h2");
+  dadJokeSection.appendChild(dadJoke);
+  dadJoke.innerText = jokes.joke;
+}
+createDadJoke();
